ad30ef22f295c4ac185b29f7193c37594626903f
yyljj <yyljj@outlook.com>
2016-05-21 17:51:48 +0800

    add gson 配置

--- src/main/java/cn/edu/cup/yb/service/LoginService.java
+++ src/main/java/cn/edu/cup/yb/service/LoginService.java
@@ -1,5 +1,9 @@
 package cn.edu.cup.yb.service;
 
+import cn.edu.cup.yb.confing.DevConfig;
+import cn.edu.cup.yb.util.MCrypt;
+import cn.edu.cup.yb.util.SessionUser;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpSession;
@@ -12,6 +16,42 @@ public class LoginService {
     @Autowired
     private HttpSession httpSession;
 
+    public boolean isLogin() {
+        return httpSession.getAttribute("userid") != null;
+    }
+
+    public String toYibanAuth() {
+        String auth = "redirect:https://openapi.yiban.cn/oauth/authorize?client_id=" + DevConfig.client_id + "&redirect_uri=" + DevConfig.redirect_uri;
+        return auth;
+    }
+
+    public String processAuth(String verify_request) throws Exception {
+        MCrypt mCrypt = new MCrypt();
+        String res = new String(mCrypt.decrypt(verify_request));
+        if (saveSession(res)) {
+            return "redirect:/";
+        } else {
+            return "redirect:/yibanauth";
+        }
+    }
+
+    private boolean saveSession(String str) {
+        Gson gson = new Gson();
+        try {
+            SessionUser sessionUser = gson.fromJson(str, SessionUser.class);
+            httpSession.setAttribute("visit_time", sessionUser.visit_time);
+            httpSession.setAttribute("userid", sessionUser.visit_user.userid);
+            httpSession.setAttribute("username", sessionUser.visit_user.username);
+            httpSession.setAttribute("usernick", sessionUser.visit_user.usernick);
+            httpSession.setAttribute("usersex", sessionUser.visit_user.usersex);
+            httpSession.setAttribute("access_token", sessionUser.visit_oauth.access_token);
+            httpSession.setAttribute("token_expires", sessionUser.visit_oauth.token_expires);
+            return true;
+        } catch (Exception ex) {
+            //ex.printStackTrace();
+            return false;
+        }
+    }
 
 
 }
---
+++ src/main/java/cn/edu/cup/yb/util/SessionUser.java
@@ -0,0 +1,21 @@
+package cn.edu.cup.yb.util;
+
+/**
+ * Created by echao on 2016/2/24.
+ * Gson解析json,将易班返回的json解析为该类的对象
+ */
+public class SessionUser {
+    public int visit_time;
+    public VisitUser visit_user;
+    public VisitOauth visit_oauth;
+    public class VisitUser{
+        public int userid;
+        public String username;
+        public String usernick;
+        public char usersex;
+    }
+    public class VisitOauth{
+        public String access_token;
+        public int token_expires;
+    }
+}
