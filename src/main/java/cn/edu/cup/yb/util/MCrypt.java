87e661f5332841cd1e40d9d3f9822cfe52038718
yyljj <yyljj@outlook.com>
2016-05-21 21:26:10 +0800

    整理下代码

--- src/main/java/cn/edu/cup/yb/util/MCrypt.java
+++ src/main/java/cn/edu/cup/yb/util/MCrypt.java
@@ -13,7 +13,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class MCrypt {
 
-    static char[] HEX_CHARS = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+    static char[] HEX_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
     private String iv = DevConfig.client_id;//Dummy iv (CHANGE IT!)
     private IvParameterSpec ivspec;
@@ -22,8 +22,7 @@ public class MCrypt {
 
     private String SecretKey = DevConfig.AppSecret;//Dummy secretKey (CHANGE IT!)
 
-    public MCrypt()
-    {
+    public MCrypt() {
         ivspec = new IvParameterSpec(iv.getBytes());
 
         keyspec = new SecretKeySpec(SecretKey.getBytes(), "AES");
@@ -39,9 +38,8 @@ public class MCrypt {
         }
     }
 
-    public byte[] encrypt(String text) throws Exception
-    {
-        if(text == null || text.length() == 0)
+    public byte[] encrypt(String text) throws Exception {
+        if (text == null || text.length() == 0)
             throw new Exception("Empty string");
 
         byte[] encrypted = null;
@@ -50,17 +48,15 @@ public class MCrypt {
             cipher.init(Cipher.ENCRYPT_MODE, keyspec, ivspec);
 
             encrypted = cipher.doFinal(padString(text).getBytes());
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new Exception("[encrypt] " + e.getMessage());
         }
 
         return encrypted;
     }
 
-    public byte[] decrypt(String code) throws Exception
-    {
-        if(code == null || code.length() == 0)
+    public byte[] decrypt(String code) throws Exception {
+        if (code == null || code.length() == 0)
             throw new Exception("Empty string");
 
         byte[] decrypted = null;
@@ -70,31 +66,26 @@ public class MCrypt {
 
             decrypted = cipher.doFinal(hexToBytes(code));
             //Remove trailing zeroes
-            if( decrypted.length > 0)
-            {
+            if (decrypted.length > 0) {
                 int trim = 0;
-                for( int i = decrypted.length - 1; i >= 0; i-- ) if( decrypted[i] == 0 ) trim++;
+                for (int i = decrypted.length - 1; i >= 0; i--) if (decrypted[i] == 0) trim++;
 
-                if( trim > 0 )
-                {
+                if (trim > 0) {
                     byte[] newArray = new byte[decrypted.length - trim];
                     System.arraycopy(decrypted, 0, newArray, 0, decrypted.length - trim);
                     decrypted = newArray;
                 }
             }
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new Exception("[decrypt] " + e.getMessage());
         }
         return decrypted;
     }
 
 
-    public static String bytesToHex(byte[] buf)
-    {
+    public static String bytesToHex(byte[] buf) {
         char[] chars = new char[2 * buf.length];
-        for (int i = 0; i < buf.length; ++i)
-        {
+        for (int i = 0; i < buf.length; ++i) {
             chars[2 * i] = HEX_CHARS[(buf[i] & 0xF0) >>> 4];
             chars[2 * i + 1] = HEX_CHARS[buf[i] & 0x0F];
         }
@@ -103,31 +94,28 @@ public class MCrypt {
 
 
     public static byte[] hexToBytes(String str) {
-        if (str==null) {
+        if (str == null) {
             return null;
         } else if (str.length() < 2) {
             return null;
         } else {
             int len = str.length() / 2;
             byte[] buffer = new byte[len];
-            for (int i=0; i<len; i++) {
-                buffer[i] = (byte) Integer.parseInt(str.substring(i*2,i*2+2),16);
+            for (int i = 0; i < len; i++) {
+                buffer[i] = (byte) Integer.parseInt(str.substring(i * 2, i * 2 + 2), 16);
             }
             return buffer;
         }
     }
 
 
-
-    private static String padString(String source)
-    {
+    private static String padString(String source) {
         char paddingChar = 0;
         int size = 16;
         int x = source.length() % size;
         int padLength = size - x;
 
-        for (int i = 0; i < padLength; i++)
-        {
+        for (int i = 0; i < padLength; i++) {
             source += paddingChar;
         }
 
