1121719e6a373900c3a7adad5da844a5b19ad31d
yyljj <yljj@outlook.com>
2016-05-21 10:52:39 +0800

    初始项目

---
+++ src/main/java/cn/edu/cup/yb/LostandfoundApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.cup.yb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostandfoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostandfoundApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/edu/cup/yb/LostandfoundApplicationTests.java
@@ -0,0 +1,18 @@
+package cn.edu.cup.yb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LostandfoundApplication.class)
+@WebAppConfiguration
+public class LostandfoundApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
