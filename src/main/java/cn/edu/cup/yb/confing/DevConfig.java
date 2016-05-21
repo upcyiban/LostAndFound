f63a340cb70c63c1fe7f282aae526b08f68e8660
yyljj <yyljj@outlook.com>
2016-05-21 17:19:29 +0800

    add devconfig

---
+++ src/main/java/cn/edu/cup/yb/confing/DevConfig.java
@@ -0,0 +1,13 @@
+package cn.edu.cup.yb.confing;
+
+/**
+ * Created by yyljj on 2016/5/21.
+ */
+public class DevConfig {
+    public static String client_id = "2f803dad07f95108";
+    public static String redirect_uri = "http://f.yiban.cn/iapp47291";
+    public static String AppSecret = "f732a9b113713c3e1f85a85b6b1282b8";
+
+    public static String adminUsername = "admin";
+    public static String adminPassword = "admin";
+}
