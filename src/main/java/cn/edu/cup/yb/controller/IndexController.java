c0f4ee31fd3579620534fea300e34a7bd2c417dc
yyljj <yyljj@outlook.com>
2016-05-21 21:10:46 +0800

    改下端口

--- src/main/java/cn/edu/cup/yb/controller/IndexController.java
+++ src/main/java/cn/edu/cup/yb/controller/IndexController.java
@@ -1,17 +1,25 @@
 package cn.edu.cup.yb.controller;
 
 import cn.edu.cup.yb.confing.DevConfig;
+import cn.edu.cup.yb.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 
 /**
  * Created by yyljj on 2016/5/21.
  */
+@Controller
 public class IndexController {
 
     @Autowired
     private HttpSession httpSession;
+
+    @Autowired
+    private LoginService loginService;
 
     public boolean isLogin() {
         return httpSession.getAttribute("userid") != null;
@@ -21,4 +29,15 @@ public class IndexController {
         String auth = "redirect:https://openapi.yiban.cn/oauth/authorize?client_id=" + DevConfig.client_id + "&redirect_uri=" + DevConfig.redirect_uri;
         return auth;
     }
+
+    @RequestMapping("/")
+    public String showIndex(Model model){
+        if (!loginService.isLogin()){
+            return loginService.toYibanAuth();
+        }
+
+
+       return "index";
+    }
+
 }
