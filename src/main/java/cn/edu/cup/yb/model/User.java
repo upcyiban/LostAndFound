65a58c6a27e9559698e1efad81c2b823d03424e9
yyljj <yyljj@outlook.com>
2016-05-21 11:55:46 +0800

    fix error

--- src/main/java/cn/edu/cup/yb/model/User.java
+++ src/main/java/cn/edu/cup/yb/model/User.java
@@ -1,7 +1,18 @@
 package cn.edu.cup.yb.model;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by yyljj on 2016/5/21.
  */
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
 }
