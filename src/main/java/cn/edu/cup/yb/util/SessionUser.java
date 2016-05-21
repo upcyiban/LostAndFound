0fe701fb8fd98b46df3285ce308eca9b0564c368
yyljj <yljj@outlook.com>
2016-05-21 21:26:10 +0800

    整理下代码

--- src/main/java/cn/edu/cup/yb/util/SessionUser.java
+++ src/main/java/cn/edu/cup/yb/util/SessionUser.java
@@ -8,13 +8,15 @@ public class SessionUser {
     public int visit_time;
     public VisitUser visit_user;
     public VisitOauth visit_oauth;
-    public class VisitUser{
+
+    public class VisitUser {
         public int userid;
         public String username;
         public String usernick;
         public char usersex;
     }
-    public class VisitOauth{
+
+    public class VisitOauth {
         public String access_token;
         public int token_expires;
     }
