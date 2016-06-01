3ffacefb8b31fa54d696eb67838a488aad64b6c6
yyljj <yyljj@outlook.com>
2016-06-01 23:22:21 +0800

    用户发布逻辑部分基本完成.

---
+++ src/main/java/cn/edu/upc/yb/controller/UserController.java
@@ -0,0 +1,7 @@
+package cn.edu.upc.yb.controller;
+
+/**
+ * Created by yyljj on 16-6-1.
+ */
+public class UserController {
+}
---
+++ src/main/java/cn/edu/upc/yb/service/UserService.java
@@ -0,0 +1,7 @@
+package cn.edu.upc.yb.service;
+
+/**
+ * Created by yyljj on 16-6-1.
+ */
+public class UserService {
+}
